8941e1b1c9215f295b712dfe3b9a79f1c8a4cc0f
Joejoe <82959624+joejoe135791@users.noreply.github.com>
2022-11-06 19:56:45 -0800

    Discord bot upload with custom api key.
    Code provided by carterbot and the carterbot team

---
+++ discord-bot/events/interactionCreate.js
@@ -0,0 +1,23 @@
+const { ActionRowBuilder, ButtonBuilder, ButtonStyle } = require('discord.js');
+const config = require('../config.json');
+
+module.exports = {
+	name: 'interactionCreate',
+	async execute(interaction) {
+
+        if (interaction.isChatInputCommand()) {
+            const command = interaction.client.commands.get(interaction.commandName);
+
+            if (!command) return;
+
+            try {
+                await command.execute(interaction);
+            } catch (error) {
+                console.error(error);
+                await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+            }
+            console.log(`${interaction.user.tag} in #${interaction.channel.name} triggered an interaction.`);
+        }
+    },
+};
+
---
+++ discord-bot/events/messageCreate.js
@@ -0,0 +1,61 @@
+const { ActionRowBuilder, ButtonBuilder, ButtonStyle, ComponentType } = require('discord.js');
+const { Carter } = require('carter-js');
+const config = require('../config.json');
+
+module.exports = {
+	name: 'messageCreate',
+	async execute(message) {	
+		if(message.author.id === config.clientId) return; // Returns if message is itself
+
+		if(message.channel.isDMBased() || config.channelmode === "on" && message.channel.id === config.channelid) { // Checks if the message comes from a DM, or if (when the config is on) it came from a specific channel
+			try {
+				await message.channel.sendTyping()
+
+				const options = {
+					uuid: message.author.id // Optional
+				}
+
+				const usermsg = await message.content // Defines the provided message query
+				const carter = new Carter(config.carterkey)
+				const response = await carter.say(usermsg, options)
+				const reply_message = response.data.output.text
+
+				const row1 = new ActionRowBuilder() // Downvote button builder
+				.addComponents(
+					new ButtonBuilder()
+						.setCustomId('message_downvote')
+						.setLabel('Downvote')
+						.setEmoji('⬇️')
+						.setStyle(ButtonStyle.Secondary),
+				);
+				
+				const msg = await message.reply({ content: `${reply_message}`, components: [row1], allowedMentions: { repliedUser: false } }) // Sends response to channel
+
+				const collector = msg.createMessageComponentCollector({ componentType: ComponentType.Button, time: 15000 });
+				collector.on('collect', async i => {
+					await carter.downvote(response)
+
+				const row1 = new ActionRowBuilder() // Downvote button builder
+					.addComponents(
+						new ButtonBuilder()
+						.setCustomId('message_downvote')
+						.setLabel('Downvote')
+						.setEmoji('⬇️')
+						.setStyle(ButtonStyle.Secondary)
+						.setDisabled(true),
+					);	
+					await i.update({ content: `\`DOWNVOTED\` || ${reply_message}`, components: [row1] });
+				});
+
+				await collector.on('end', collected => {
+					msg.edit({ components: [] });
+				});
+
+			} catch(error) {
+				console.log(error)
+			}
+		} else {
+			return;
+		}
+	},
+};
